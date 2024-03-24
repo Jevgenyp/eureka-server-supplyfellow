19f41b6fe46b0925036ceebe009a9734a8ce1ea1
Jevgenijs Pitkevics <Jpcoatingss@gmail.com>
2024-03-24 23:57:20 +0100

    Initial commit

---
+++ src/main/java/jpi/supplyfellow/eurekaserversupplyfellow/EurekaServerSupplyfellowApplication.java
@@ -0,0 +1,13 @@
+package jpi.supplyfellow.eurekaserversupplyfellow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerSupplyfellowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerSupplyfellowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jpi/supplyfellow/eurekaserversupplyfellow/EurekaServerSupplyfellowApplicationTests.java
@@ -0,0 +1,13 @@
+package jpi.supplyfellow.eurekaserversupplyfellow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerSupplyfellowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
