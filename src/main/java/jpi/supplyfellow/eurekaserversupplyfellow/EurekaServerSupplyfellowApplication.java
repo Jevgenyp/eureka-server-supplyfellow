dbf0480098cba84828a6c1cf322cda29f6bf9cc7
Jevgenijs Pitkevics <Jpcoatingss@gmail.com>
2024-03-25 19:07:30 +0100

    README.md added

--- src/main/java/jpi/supplyfellow/eurekaserversupplyfellow/EurekaServerSupplyfellowApplication.java
+++ src/main/java/jpi/supplyfellow/eurekaserversupplyfellow/EurekaServerSupplyfellowApplication.java
@@ -2,12 +2,14 @@ package jpi.supplyfellow.eurekaserversupplyfellow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerSupplyfellowApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerSupplyfellowApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerSupplyfellowApplication.class, args);
+    }
 
 }
